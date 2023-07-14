0f7243f0a83f660de00f3c18aa0639ae62f2415d
NIkhil <knewboy.nykhil@gmail.com>
2023-07-14 14:55:46 +0545

    connected successfully wit hdatabase

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -9,3 +10,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+mongoose.
+connect('mongodb+srv://Haubaa:hauba123456@haubaapi.mipwaf4.mongodb.net/Hauba-API?retryWrites=true&w=majority')
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...'));
